2da601319c06665d295135966b32dee0f27cf81c
Stefanie Satna <satnastefani@gmail.com>
2021-02-19 16:13:12 +0000

    Adds cheering messages for different ketosis streaks

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -13,6 +13,18 @@ function HomeScreen() {
     alignItems: "center",
   };
 
+  let cheering;
+  if (daysInKetosis === 0) {cheering = `Today is the day to start! 🎯`} else
+  if (daysInKetosis === 1) {cheering = `First day in! Purple pee FTW 💜`} else
+  if (daysInKetosis === 5) {cheering = `It's what's on the stick that counts 💪`} else
+  if (daysInKetosis === 10) {cheering = `Let the stick do the talking 😎`} else
+  if (daysInKetosis === 15) {cheering = `If bread was allowed, everyone could do it. Keep it up 🎉`} else
+  if (daysInKetosis === 20) {cheering = `To reach inner peace, you shall purple piss 💦`} else
+  if (daysInKetosis > 20 && daysInKetosis < 30)  {cheering = `Day ${daysInKetosis}. Purpee presses F 🎖️`} else
+  if (daysInKetosis > 30) {cheering = `Day ${daysInKetosis}. You are the real keto lord 🔥`} else {
+    cheering = `${daysInKetosis} straight days in ketosis 💜`
+  }
+
   return (
     <Container>
       <View style={contentContainer}>
@@ -20,7 +32,7 @@ function HomeScreen() {
       </View>
       <View style={styleBottomView}>
         <Text style={mainText}>
-          {daysInKetosis} straight days in ketosis 💜
+          {cheering}
         </Text>
       </View>
     </Container>
