587a7bed4b937220cf187ebca06d345b0ca6ced9
Stefanie Satna <satnastefani@gmail.com>
2021-03-02 20:53:44 +0000

    Delete old clock-widget dependencies

--- components/screens/ReminderScreen.js
+++ components/screens/ReminderScreen.js
@@ -1,10 +1,6 @@
 import React from "react";
-import { Text, View, Switch } from "react-native";
-import ReminderWidget from "../ReminderWidget"
-
-import Container from "../Container";
-import ClockWidget from "../ClockWidget"
-import { contentContainer, mainText } from "../../data/Style";
+import { Text } from "react-native"
+// import ReminderWidget from "../ReminderWidget"
 
 function ReminderScreen() {
   const styleDailyLog = {
@@ -17,24 +13,8 @@ function ReminderScreen() {
   };
 
   return (
-    <ReminderWidget />
+    <Text>Reminder</Text>
   )
 }
-
-// return (
-//   <ClockWidget />
-//   // <Container>
-//     {/* <View style={contentContainer}>
-//       <View style={styleDailyLog}>
-//         <Text style={mainText}>Daily log reminder</Text>
-//         <Switch />
-//       </View>
-//       <View>
-//         <Text>It's best to log your ketone levels first thing in the morning.</Text>
-//       </View>
-//       <ClockWidget></ClockWidget>
-//     </View>
-//   </Container> */}
-// );
 
 export default ReminderScreen;
