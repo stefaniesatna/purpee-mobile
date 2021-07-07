0e8a4d02f6eabb89f18ca33367069aa6c4026c2b
Stefanie Satna <satnastefani@gmail.com>
2021-07-07 12:35:12 +0200

    Refactor motivating messages so that they have a system to add new ones and get rid of complicated conditionals

---
+++ data/MotivatingMessagesDB.js
@@ -0,0 +1,25 @@
+export let motivatingMessages = [];
+
+// newMessage takes in motivational message, and starting day (inclusive), ending day (exclusive, optional) of when it should display
+export function newMessage(m, min, max = min + 1) {
+  // TODO: Maybe add a catch if missing m or min
+  if (typeof min !== "number" || min < 0) {
+    console.log("Min needs to be a positive integer");
+    return;
+  }
+
+  // If there already is a message with the same starting day, rewrite it
+  motivatingMessages.forEach((mes) => {
+    if (mes.min === min) {
+      (mes.max = max), (mes.message = m);
+      return;
+    }
+  });
+
+  let message = {
+    min: min,
+    max: max,
+    message: m,
+  };
+  motivatingMessages.push(message);
+}
