8aceda8253005deb6333d6c39ea992813a22d2ef
Stefanie Satna <satnastefani@gmail.com>
2021-07-07 10:02:14 +0200

    Install expo-google-fonts for custom fonts, add montserrat

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as Notifications from "expo-notifications";
 import { StatusBar } from "expo-status-bar";
+import AppLoading from 'expo-app-loading';
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -10,6 +11,28 @@ import { MainStackScreen } from "./components/navigation/MainStackScreen";
 import { LevelProvider } from "./LevelContext";
 import { NotificationProvider } from "./NotificationContext";
 import { NotificationWrapper } from "./NotificationWrapper";
+
+import {
+  useFonts,
+  Montserrat_100Thin,
+  Montserrat_100Thin_Italic,
+  Montserrat_200ExtraLight,
+  Montserrat_200ExtraLight_Italic,
+  Montserrat_300Light,
+  Montserrat_300Light_Italic,
+  Montserrat_400Regular,
+  Montserrat_400Regular_Italic,
+  Montserrat_500Medium,
+  Montserrat_500Medium_Italic,
+  Montserrat_600SemiBold,
+  Montserrat_600SemiBold_Italic,
+  Montserrat_700Bold,
+  Montserrat_700Bold_Italic,
+  Montserrat_800ExtraBold,
+  Montserrat_800ExtraBold_Italic,
+  Montserrat_900Black,
+  Montserrat_900Black_Italic,
+} from '@expo-google-fonts/montserrat';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -22,21 +45,46 @@ Notifications.setNotificationHandler({
 const Root = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <LevelProvider>
-        <NotificationProvider>
-          {/* Notifications need to receive context from NotificationProvider, but also need to wrap the rest of the app. 
-          That's why most of code related to notifications is in NotificationsWrapper */}
-          <NotificationWrapper>
-            <StatusBar style="light" />
-            <Root.Navigator mode="modal" headerMode="none">
-              <Root.Screen name="Main" component={MainStackScreen} />
-              <Root.Screen name="Modal" component={ModalScreen} />
-            </Root.Navigator>
-          </NotificationWrapper>
-        </NotificationProvider>
-      </LevelProvider>
-    </NavigationContainer>
-  );
+  let [fontsLoaded] = useFonts({
+    Montserrat_100Thin,
+    Montserrat_100Thin_Italic,
+    Montserrat_200ExtraLight,
+    Montserrat_200ExtraLight_Italic,
+    Montserrat_300Light,
+    Montserrat_300Light_Italic,
+    Montserrat_400Regular,
+    Montserrat_400Regular_Italic,
+    Montserrat_500Medium,
+    Montserrat_500Medium_Italic,
+    Montserrat_600SemiBold,
+    Montserrat_600SemiBold_Italic,
+    Montserrat_700Bold,
+    Montserrat_700Bold_Italic,
+    Montserrat_800ExtraBold,
+    Montserrat_800ExtraBold_Italic,
+    Montserrat_900Black,
+    Montserrat_900Black_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else { 
+    return (
+      <NavigationContainer>
+        <LevelProvider>
+          <NotificationProvider>
+            {/* Notifications need to receive context from NotificationProvider, but also need to wrap the rest of the app. 
+            That's why most of code related to notifications is in NotificationsWrapper */}
+            <NotificationWrapper>
+              <StatusBar style="light" />
+              <Root.Navigator mode="modal" headerMode="none">
+                <Root.Screen name="Main" component={MainStackScreen} />
+                <Root.Screen name="Modal" component={ModalScreen} />
+              </Root.Navigator>
+            </NotificationWrapper>
+          </NotificationProvider>
+        </LevelProvider>
+      </NavigationContainer>
+    );
+  }
 }
