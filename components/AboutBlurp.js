990b3571d82386a08b38177e9a44f9fc0405eea3
Stefanie Satna <satnastefani@gmail.com>
2021-07-28 15:53:37 +0100

    Change link in AboutBlurp to open page as a modal

--- components/AboutBlurp.js
+++ components/AboutBlurp.js
@@ -1,47 +1,58 @@
-import React from "react"
-import { View, Text, Dimensions, TouchableOpacity } from "react-native"
-import * as Linking from 'expo-linking';
+import React from "react";
+import { View, Text, Dimensions } from "react-native";
+import * as WebBrowser from "expo-web-browser";
 import { Ionicons } from "@expo/vector-icons";
-import { UIColors, secondaryText } from "../data/Style"
-import { Anchor } from "./Anchor"
+import { UIColors, secondaryText } from "../data/Style";
 
 export const AboutBlurp = () => {
-    const screenWidth = Dimensions.get("window").width;
-    const styleWrapper = {
-        width: screenWidth * 0.94,
-        marginBottom: 28,
-        justifySelf: "flex-end",
-        alignItems: "center",
-        justifyContent: "space-between"
-    }
-    const styleText = {
-        ...secondaryText,
-        textAlign: "center"
-    }
+  const screenWidth = Dimensions.get("window").width;
 
-    const styleLink = {
-        ...styleText,
-        textDecorationLine: "underline"
-    }
+  const styleWrapper = {
+    width: screenWidth * 0.94,
+    marginBottom: 28,
+    justifySelf: "flex-end",
+    alignItems: "center",
+    justifyContent: "space-between",
+  };
+  const styleText = {
+    ...secondaryText,
+    textAlign: "center",
+  };
 
-    const styleSecondWrapper = {
-        flexDirection: "row"
-    }
+  const styleLink = {
+    ...styleText,
+    textDecorationLine: "underline",
+  };
 
-    return (
-        <View style={styleWrapper}>
-            <View style={{marginBottom: 5}}>
-                <Ionicons name={"logo-github"} color={UIColors.blueFull} size={30} />
-            </View>
-            <Text style={styleText}>
-                Purpee is made and maintained by
-                <Text style={styleText}> </Text>
-                <Text style={styleLink} onPress={() => Linking.openURL("https://www.google.com/")}>@stefaniesatna</Text>
-                . See the code on
-                <Text style={styleText}> </Text>
-                <Text style={styleLink} onPress={() => Linking.openURL("https://www.google.com/")}>GitHub</Text>
-                .
-            </Text>
-        </View>
-    )
-}
+  return (
+    <View style={styleWrapper}>
+      <View style={{ marginBottom: 5 }}>
+        <Ionicons name={"logo-github"} color={UIColors.blueFull} size={30} />
+      </View>
+      <Text style={styleText}>
+        Purpee is made and maintained by
+        <Text style={styleText}> </Text>
+        <Text
+          style={styleLink}
+          onPress={() =>
+            WebBrowser.openBrowserAsync("https://github.com/stefaniesatna")
+          }
+        >
+          @stefaniesatna
+        </Text>
+        . See the code on
+        <Text style={styleText}> </Text>
+        {/* TODO: Swap for a link to Purpee code */}
+        <Text
+          style={styleLink}
+          onPress={() =>
+            WebBrowser.openBrowserAsync("https://github.com/stefaniesatna")
+          }
+        >
+          GitHub
+        </Text>
+        .
+      </Text>
+    </View>
+  );
+};
