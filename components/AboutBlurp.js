a80da907569b7d24861b059ac1213e6405344872
Stefanie Satna <satnastefani@gmail.com>
2021-07-28 11:27:11 +0100

    Add links to AboutBlurp

--- components/AboutBlurp.js
+++ components/AboutBlurp.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { View, Text, Dimensions } from "react-native"
+import { View, Text, Dimensions, TouchableOpacity } from "react-native"
+import * as Linking from 'expo-linking';
 import { Ionicons } from "@expo/vector-icons";
 import { UIColors, secondaryText } from "../data/Style"
 import { Anchor } from "./Anchor"
@@ -13,13 +14,34 @@ export const AboutBlurp = () => {
         alignItems: "center",
         justifyContent: "space-between"
     }
+    const styleText = {
+        ...secondaryText,
+        textAlign: "center"
+    }
+
+    const styleLink = {
+        ...styleText,
+        textDecorationLine: "underline"
+    }
+
+    const styleSecondWrapper = {
+        flexDirection: "row"
+    }
 
     return (
         <View style={styleWrapper}>
             <View style={{marginBottom: 5}}>
                 <Ionicons name={"logo-github"} color={UIColors.blueFull} size={30} />
             </View>
-            <Anchor href={"www.google.com"} style={{...secondaryText, textAlign: "center"}}>Purpee is made and maintained by @stefaniesatna. See the code on GitHub.</Anchor>
+            <Text style={styleText}>
+                Purpee is made and maintained by
+                <Text style={styleText}> </Text>
+                <Text style={styleLink} onPress={() => Linking.openURL("https://www.google.com/")}>@stefaniesatna</Text>
+                . See the code on
+                <Text style={styleText}> </Text>
+                <Text style={styleLink} onPress={() => Linking.openURL("https://www.google.com/")}>GitHub</Text>
+                .
+            </Text>
         </View>
     )
 }
