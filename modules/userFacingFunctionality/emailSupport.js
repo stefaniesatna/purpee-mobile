137a897b761a75adefbd349ea0193966b1375136
Stefanie Satna <satnastefani@gmail.com>
2021-07-29 10:41:54 +0100

    Add a real support email address

--- modules/userFacingFunctionality/emailSupport.js
+++ modules/userFacingFunctionality/emailSupport.js
@@ -1,6 +1,5 @@
-//TODO Swap email address for a real one 
 import * as Linking from 'expo-linking';
 
 export const emailSupport = () => {
-    Linking.openURL('mailto:support@example.com')
+    Linking.openURL('mailto:support@purpee.app')
 }
