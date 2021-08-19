d2847bea64db65f503b823944e1db0904dbb77cf
Stefanie Satna <satnastefani@gmail.com>
2021-08-19 11:21:37 +0100

    Return NotificationWrapper to previous version, leave the fix as a todo

--- notifications/NotificationWrapper.js
+++ notifications/NotificationWrapper.js
@@ -1,16 +1,13 @@
 import React, { useRef, useEffect, useContext } from "react";
 import { NotificationContext } from "./NotificationContext";
-import { LevelContext } from "../LevelContext";
 import Constants from "expo-constants";
 import * as Notifications from "expo-notifications";
 import { degreesToTime } from "../modules/degreesToTime";
-import { formatDateYYYYMMDD } from "../modules/formatDateYYYYMMDD";
 
 export const NotificationWrapper = ({ children }) => {
   const notificationListener = useRef();
   const responseListener = useRef();
   const [notification] = useContext(NotificationContext);
-  const [levelDates] = useContext(LevelContext);
 
   const time = {
     hour: notification ? degreesToTime(notification.angle).hours : 0,
@@ -18,17 +15,6 @@ export const NotificationWrapper = ({ children }) => {
   };
 
   const isOn = notification ? notification.isOn : false;
-  const numberOfNotifications = 30
-  const intervalSecs = 20
-  const today = new Date()
-    console.log("==================\n\n\n\n")
-    console.log("Today is ", today)
-  const tomorrow = new Date()
-  tomorrow.setDate(tomorrow.getDate() + 1)
-    console.log("Today still is ", today)
-    console.log("Tomorrow is now ", tomorrow)
-  const startDate = levelDates && levelDates[formatDateYYYYMMDD(today)] > 0 ? tomorrow : today
-    console.log("Start date", startDate)
 
   useEffect(() => {
     registerForPushNotificationsAsync();
@@ -93,7 +79,7 @@ async function registerForPushNotificationsAsync() {
       finalStatus = status;
     }
     if (finalStatus !== "granted") {
-      alert("Permission for push notifications could not be granted. Reminder notifications will not be sent");
+      alert("Unable to set notifications");
       return;
     }
     token = (await Notifications.getExpoPushTokenAsync()).data;
